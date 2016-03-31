9621992c1fa97e1e689cdc70ef3160b1ec912cea
CKrishnaDev <ckdeveloptkalvinkrishna@gmail.com>
2016-04-01 03:24:42 +0700

    Added files via upload

---
+++ statuspengiriman.php
@@ -0,0 +1,74 @@
+<br>
+<fieldset class="well the-fieldset borderblackfield">
+
+	 <legend class='text-center wtf' style='color:#ff9900;'><span style='background:#E7E7E7;'>Data Pembeli</span></legend>
+
+	<div class='row identitas' style='margin-top:-20px;'>
+									<div class='text-center head'>
+										<div class='col-lg-4 col-md-4 col-sm-4 col-xs-12'>
+											<span>Nama: Customer Satu</span>
+										</div>
+										<div class='col-lg-4 col-md-3 col-sm-3 col-xs-12'>
+											<span>Invoice: AB5EI</span>
+										</div>
+										<div class='col-lg-4 col-md-5 col-sm-4 col-xs-12'>
+											<span>Tgl. Transaksi: 15-03-2016</span>
+										</div>
+									</div>
+									
+			</div>
+	<div class='row'>
+		
+		<div style='width:30%; margin:auto;'>
+			<h4 style='font-size:14px;'><span>Status:</span> <span>Transaksi Selesai</span></h4>
+			<h4 style='font-size:14px;'><span>Tanggal:</span> <span>16-03-2016</span></h4>
+		</div>
+		<br>
+	<center>
+		<div class='col-xs-12 col-md-12 col-sm-12 center-block'>
+			<button class='btn btn-danger'>Tutup Transaksi</button>
+		</div>
+	</center>
+				
+			
+	</div>
+
+
+</fieldset>
+
+<fieldset class="well the-fieldset borderblackfield" style='padding:0px;'>
+	 <legend class='text-center' style='color:#ff9900;'><span style='background:#E7E7E7;'>Info Status</span></legend>
+	 <div class='row'>
+	 	<div class='col-md-12 col-xs-12'>
+	 		<table class='table table-striped table-bordered'>
+		 		<thead class='text-center'>
+		 				<td>Tanggal Status</td>
+		 				<td>Posisi</td>
+		 				<td>Detail Status</td>
+		 		</thead>
+		 		<tbody>
+		 			<tr>
+		 				<td><span>13-03-2016</span> <span>21:23:21</span></td>
+		 				<td><span>Ekspedisi</span></td>
+		 				<td><span>Order dikirim kepada pembeli</span></td>
+		 			</tr>
+		 			<tr>
+		 				<td><span>13-03-2016</span> <span>21:23:21</span></td>
+		 				<td><span>Penjual</span></td>
+		 				<td><span>Order diterima</span></td>
+		 			</tr>
+		 			<tr>
+		 				<td><span>13-03-2016</span> <span>21:23:21</span></td>
+		 				<td><span>Ekspedisi</span></td>
+		 				<td><span>Melakukan Check Out</span></td>
+		 			</tr>
+		 		</tbody>	
+	 		</table>	
+
+	 	</div>
+	 	 
+
+	 </div>
+	
+	
+</fieldset>
